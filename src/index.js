83bc9e371b3b5512e76dddb66a853773f77ffcf7
Subash Khatri <subashkhatri44@gmail.com>
2022-01-27 12:41:42 -0600

    worked on setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {logger} from 'redux-logger';
+import  createSagaMiddleware from 'redux-saga';
+import {Provider} from 'react-redux';
+import {takeEvery, put} from 'redux-saga/effects'
+import axios from 'axios';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({ 
+      }),
+    applyMiddleware(sagaMiddleware, logger),
+  
+  );
+    sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
