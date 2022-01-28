fa014226e4b9f8ef1a542d6d0388e9fe2d88b02f
Desiree Quade <nekodez@gmail.com>
2022-01-28 10:21:49 -0600

    created category component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+import './App.css';
+
 import Home from '../Home/Home';
+import Categories from '../Categories/Categories';
 
 function App(props) {
   return (
   
     <div>
-      <Home />
+      <Router>
+        <nav>
+          <Link to="/Home" className="link">Home</Link>
+          <Link to="/Categories" className="link">Categories</Link>
+        </nav>
+        <Route path="/Home" exact>
+          <Home />
+        </Route>
+        
+        <Route path="/Categories">
+          <Categories />
+        </Route>
+        
+      </Router>
     </div>
   
   );
