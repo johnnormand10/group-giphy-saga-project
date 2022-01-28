fbd91fac1f8dd7364fe96e0102497a1b53acc1a2
Subash Khatri <subashkhatri44@gmail.com>
2022-01-28 10:51:57 -0600

    worked on FavoriteList

---
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import FavoriteItem from '../FavoriteItem/FavoriteItem';
+
+const FavoriteList = () => {
+    useEffect 
+
+    // gain access to search reducer
+    const favoriteResults = useSelector(store => store.getFavorite);
+    console.log('findIt in search list', favoriteResults);
+    // loop through search reducer
+
+
+return (
+    <>
+    <div>
+        {favoriteResults.map((url) => (
+            <FavoriteItem url={url.url} alt ={url.name} key ={url.id}/>
+        ))}
+    </div>
+    </>
+)
+}
+
+export default FavoriteList;
